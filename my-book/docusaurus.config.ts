f421b3b308c81b40b191008866ec38f974d6ad2d
Saima <160252804+SaimaWaheed-Student@users.noreply.github.com>
2026-01-02 03:26:51 -0800

    Change baseUrl to root for site deployment
    Updated baseUrl for GitHub pages deployment.

--- my-book/docusaurus.config.ts
+++ my-book/docusaurus.config.ts
@@ -18,7 +18,7 @@ const config: Config = {
   url: 'https://saimawaheed-student.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/Hackathon01-physical-ai-humonaid-book/',
+  baseUrl: '/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
